66b91d8d9c7c395eb019a2e5ad296ff0275fd5f5
Holly <hp01674@georgiasouthern.edu>
2020-01-17 22:12:46 -0500

    Pull random fact from API

--- handler.js
+++ handler.js
@@ -1,12 +1,11 @@
 'use strict';
 
-module.exports.cat_fact = (event, context, callback) => {
-  // TODO: Write code to pull a random fact from the API.
-  // https://cat-fact.herokuapp.com/facts/random
-  // 1. Call the API
-  // 2. Parse the result
-  // 3. Put the fact in the response
-  const alexa_says = 'Hey there.'
+module.exports.cat_fact =  async (event, context, callback) => {
+  
+  let alexa_says = 'Hey there.'
+  const data = await fetch("https://cat-fact.herokuapp.com/facts/random");
+  const fact = await data.json();
+  alexa_says = fact.text
   const response = {
     version: '1.0',
     response: {
